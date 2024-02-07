25520ef1926a699a22e1ce58126f5c57ddb1ea61
Nastya <nastya.korotkikh.1984@mail.ru git config --global user.name>
2024-02-07 10:57:49 +0500

    Add test

---
+++ test/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+void PrinArray(int[] myArray, int i)
+{
+
+    if (i == 0)
+
+    {
+        Console.Write(myArray[i]);
+        return;
+    }
+    Console.Write(myArray[i] + ", ");
+    PrinArray(myArray, i - 1);
+}
+int[] myArray = { 2, 5, 3 };
+int n = myArray.Length - 1;
+PrinArray(myArray, n);
+
+
+
